e8b640f9e82597cb625918fd5a3cc4fdee3da8b1
Moisés <bk_moises@hotmail.com>
2022-10-28 22:03:44 -0300

    Menu principal v2 e inclusão de submenu

--- pim-soft.c
+++ pim-soft.c
@@ -1,99 +1,172 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <windows.h>
+#include <locale.h>
+#include <conio.h>
 
-typedef struct {
+typedef struct
+{
   char user[30];
   char pass[50];
 } Login;
 
-int main() {
+int input = 0;
+
+int main()
+{
+  setlocale(LC_ALL, "Portuguese");
+
   Login login;
   int try = 3;
 
-  printf("Bem Vindo!\n");
-  printf("Para continuar, digite seu login e senha.\n\n");
+  printf("\t\t\t\t\t\t\t\t Bem Vindo!\n");
+  printf("\t\t\t\t\t\t Pressione a tecla 'Enter' para continuar....");
+  getchar();
+  system("cls");
 
-  while (try > 0) {
-    printf("-----------------------------------------\n");
-    printf("Login: ");
+  while (try > 0)
+  {
+
+    printf("\t\t\t\t\t\t Para continuar, digite seu login e senha: \n");
+    printf("\t\t\t\t\t\t============================================\n");
+    ;
+    printf("\t\t\t\t\t\tLogin: ");
     scanf("%s", &login.user);
-    printf("Senha: ");
+    printf("\t\t\t\t\t\tSenha: ");
     scanf("%s", &login.pass);
 
-    if (strcmp(login.user, "admin") == 0 && strcmp(login.pass, "123") == 0) {
-      int menu;
+    system("cls");
+
+    if (strcmp(login.user, "admin") == 0 && strcmp(login.pass, "123") == 0)
+    {
 
       printf("Login efetuado com sucesso\n");
+      Sleep(1000);
+      system("cls");
 
-      system("sleep 3");
-      system("clear");
-
-      do {
-        system("clear");
-
-        printf("Infinite Pay\n\n");
-
-        printf("(1) Cadastrar novo cliente.\n");
-        printf("(2) Alterar plano.\n");
-        printf("(3) Consulta.\n");
-        printf("(4) Arir novo chamado.\n");
-        printf("\n(0) Sair.\n");
+      do
+      {
+        printf("\n");
+        printf("\t\t\t\t\t\t\t\t  INIFITE PAY\n");
+        printf("\n");
+        printf("\t\t\t\t\t\t============================================\n");
+        printf("\t\t\t\t\t\t|\t                                   |\n");
+        printf("\t\t\t\t\t\t|\t (1) Cadastrar novo cliente        |\n");
+        printf("\t\t\t\t\t\t|\t (2) Alterar plano.                |\n");
+        printf("\t\t\t\t\t\t|\t (3) Consulta                      |\n");
+        printf("\t\t\t\t\t\t|\t (4) Arir novo chamado             |\n");
+        printf("\t\t\t\t\t\t|\t (5) Logout                        |\n");
+        printf("\t\t\t\t\t\t|\t (0) Sair                          |\n");
+        printf("\t\t\t\t\t\t|\t                                   |\n");
+        printf("\t\t\t\t\t\t============================================\n");
+        printf("\n\n");
+        printf("\t\t\t\t\t\tSelecione uma opção: ");
         fflush(stdin);
+        scanf("%d", &input);
+        system("cls");
 
-        // menu = getchar();
-        // TODO Alterar para getchart
-        scanf("%d", &menu);
-      } while (menu != 0 && menu != 1 && menu != 2 && menu != 3 && menu != 4);
+        switch (input)
+        {
+        case 1:
+          printf("\n");
+          printf("\t\t\t\t\t\t\t  CADASTRAR CLIENTE\n");
+          printf("\n");
+          printf("\t\t\t\t\t=============================================\n");
+          printf("\t\t\t\t\t|\t                                    |\n");
+          printf("\t\t\t\t\t| Nome Completo:                            |\n");
+          getch();
+          printf("\t\t\t\t\t| Data Nascimento:                          |\n");
+          getch();
+          printf("\t\t\t\t\t| Sexo:                                     |\n");
+          getch();
+          printf("\t\t\t\t\t| CPF:                                      |\n");
+          getch();
+          printf("\t\t\t\t\t| Endereço:                                 |\n");
+          getch();
+          printf("\t\t\t\t\t| Plano:                                    |\n");
+          printf("\t\t\t\t\t|    (1) Bronze                             |\n");
+          printf("\t\t\t\t\t|    (2) Prata                              |\n");
+          printf("\t\t\t\t\t|    (3) Ouro                               |\n");
+          getch();
+          fflush(stdin);
+          system("cls");
+          break;
+        case 2:
+          printf("\n");
+          printf("\t\t\t\t\t\t\t  ALTERAR PLANO\n");
+          printf("\n");
+          printf("\t\t\t\t\t=============================================\n");
+          printf("\t\t\t\t\t| Plano Atual: Prata                        |\n");
+          printf("\t\t\t\t\t=============================================\n");
+          printf("\t\t\t\t\t| Selecione o novo plano:                   |\n");
+          printf("\t\t\t\t\t|                                           |\n");
+          printf("\t\t\t\t\t| (1) Bronze                                |\n");
+          printf("\t\t\t\t\t| (2) Ouro                                  |\n");
+          printf("\t\t\t\t\t|                                           |\n");
+          printf("\t\t\t\t\t|                                           |\n");
+          printf("\t\t\t\t\t| (0) Cancelar                              |\n");
+          getch();
+          fflush(stdin);
+          system("cls");
+          break;
+        case 3:
+          printf("\n");
+          printf("\t\t\t\t\t\t\t  CONSULTAR\n");
+          printf("\n");
+          printf("\t\t\t\t\t=============================================\n");
+          printf("\t\t\t\t\t| Digite o CPF:                             |\n");
+          printf("\t\t\t\t\t=============================================\n");
+          printf("\n");
+          getch();
+          fflush(stdin);
+          system("cls");
+          break;
+        case 4:
+          printf("\n");
+          printf("\t\t\t\t\t\t\t  NOVO CHAMADO\n");
+          printf("\n");
+          printf("\t\t\t\t\t=============================================\n");
+          printf("\t\t\t\t\t|\t                                    |\n");
+          printf("\t\t\t\t\t| Nome Completo:                            |\n");
+          getch();
+          printf("\t\t\t\t\t| Telefone:                                 |\n");
+          getch();
+          printf("\t\t\t\t\t| E-mail:                                   |\n");
+          getch();
+          printf("\t\t\t\t\t| Descrição:                                |\n");
+          getch();
+          fflush(stdin);
+          system("cls");
+          break;
+        case 5:
+          input = 0;
+          break;
+        case 0:
+          exit(EXIT_SUCCESS);
+        default:
+          printf("\n\t\t\tOpção Invalida!\n\n");
+          fflush(stdin);
+        }
 
-      switch (menu) {
-      case 1:
-        printf("Cadastrar novo cliente\n");
-        printf("Nome Completo: \n");
-        printf("Data Nascimento: \n");
-        printf("Sexo: \n");
-        printf("Endereço: \n");
-        system("clear");
-        printf("Plano: \n");
-        printf(": \n");
-        printf(": \n");
-        printf(": \n");
-        break;
-
-      case 2:
-        printf("Alterar plano");
-        printf(": \n");
-        break;
-
-      case 3:
-        printf("Consulta");
-        break;
-
-      case 4:
-        printf("Abrir novo chamado");
-        break;
-
-      case 0:
-        system("exit");
-        break;
-
-      default:
-        printf("Opção Invalida!");
-      }
-
-      return 0;
-    } else {
-      if (try > 0) {
+      } while (input != 0);
+    }
+    else
+    {
+      if (try > 0)
+      {
         printf("\nUsuario ou senha incorretos. Tente Novamente!\n");
         try--;
       }
-      if (try == 1) {
-        printf("O programa será encerrado caso erre a senha novamente.\n");
+      if (try == 1)
+      {
+        printf("O programa será encerrado caso erre a senha novamente.\n\n");
       }
     }
   }
-  system("clear");
+
+  system("cls");
   printf("Número de tentativas excedida.\nFinalizando...\n");
-  system("sleep 3");
+  Sleep(3000);
   return -1;
 }
